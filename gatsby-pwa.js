f2ebf4871bab62ddb6c73c8fd9de8b08b2e3035d
Mateusz Kapeluszny <mkapeluszny@gmail.com>
2020-10-19 13:40:08 +0200

    fix(pwa): fixed fetchevent error

--- gatsby-pwa.js
+++ gatsby-pwa.js
@@ -8,15 +8,16 @@ module.exports = [
             background_color: `#f7f0eb`,
             theme_color: `#a2466c`,
             display: `standalone`,
-            icon: './src/favicon.svg',
+            icon: require.resolve('./icons/favicon.svg'),
             cache_busting_mode: 'none'
         },
     },
     {
         resolve: 'gatsby-plugin-offline',
         options: {
+            appendScript: require.resolve(`./src/service-worker.js`),
             workboxConfig: {
-                globPatterns: ['**/icon-path*']
+                globPatterns: ['**/*']
             }
         }
     }
